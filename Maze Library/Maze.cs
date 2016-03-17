8de93bf80ecc07c947e379fd38f7fb377723088c
vasuperniss <vasuperniss@gmail.com>
2016-03-17 19:55:42 +0200

    Re-Commit the Maze interface (failed the first time)

---
+++ Maze Library/Maze.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Maze_Library
+{
+    public interface Maze
+    {
+        MazePosition getStartPosition();
+
+        MazePosition getFinishPosition();
+
+        List<MazePosition> getAvailablePositionsFrom(MazePosition position);
+    }
+}
