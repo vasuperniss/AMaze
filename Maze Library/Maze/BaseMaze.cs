fd1651022a97fe338193cefdfcb725c4008e8d3d
vasuperniss <vasuperniss@gmail.com>
2016-03-23 16:59:14 +0200

    Added some small changes to the maze library

---
+++ Maze Library/Maze/BaseMaze.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze_Library.Maze
+{
+    public abstract class BaseMaze : IMaze
+    {
+        protected MazePosition startPosition;
+        protected MazePosition endPosition;
+
+        public abstract List<MazePosition> getAvailablePositionsFrom(MazePosition position);
+
+        public MazePosition getFinishPosition()
+        {
+            return this.endPosition;
+        }
+
+        public MazePosition getStartPosition()
+        {
+            return this.startPosition;
+        }
+    }
+}
